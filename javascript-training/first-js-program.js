878dfd46b0f479fd7fedc05756d96406c09f5285
Asha <aashalogeshwaran@gmail.com>
2026-06-08 23:46:48 +0530

    Assignment1

---
+++ javascript-training/first-js-program.js
@@ -0,0 +1,10 @@
+console.log("hello world!!");
+console.info("this is an information message");
+console.warn("this is a warning message");
+console.error("this is an error message..");
+sumofnum(5, 6);
+
+function sumofnum(a, b){
+    c = a + b;
+    console.log(c);
+}; 
---
+++ typeScript-training/Datatypes.ts
@@ -0,0 +1,9 @@
+//primitive data type (immutable) {number, string, boolean, undefined, null, symbol}
+
+//number
+
+let empIds:number = 1234;
+console.log(empIds);
+
+let age:undefined ;
+console.log(typeof age);
